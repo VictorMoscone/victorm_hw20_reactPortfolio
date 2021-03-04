97c6ad1ec7e34a28ed476f74b66e3b2109529608
Victor Moscone <victormoscone@gmail.com>
2021-03-03 22:30:59 -0800

    Style: Added background styling and text styling to Nav Bar.

--- client/src/components/Nav/NavBar.js
+++ client/src/components/Nav/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./styles.css";
 
 
 const NavBar = () => {
@@ -8,17 +9,17 @@ const NavBar = () => {
         <div>
             <Nav className="justify-content-center" activeKey="/home">
                 <Nav.Item>
-                    <Nav.Link href="/home">Home</Nav.Link>
+                    <Nav.Link href="/home" className="textColor">Home</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href="/about">About</Nav.Link>
+                    <Nav.Link href="/about" className="textColor">About</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href="/contact">Contact</Nav.Link>
+                    <Nav.Link href="/contact" className="textColor">Contact</Nav.Link>
                 </Nav.Item>
             </Nav>
         </div>
-    )
-}
+    );
+};
 
 export default NavBar
