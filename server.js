1cf2fe45042f9f3c131e7e6ecb2018a8ff46122e
Victor Moscone <victormoscone@gmail.com>
2021-03-03 19:09:55 -0800

    Feat: Server.js given a basic setup.

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// After you run it, creates index.html file inside the "Build" folder.
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+};
+
+app.listen(PORT, () => 
+    console.log(`Listening at http://localhost:${PORT}`)
+);
