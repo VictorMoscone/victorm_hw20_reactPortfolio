14290b375386e5d085a52887c2b8f99e76e0d440
Victor Moscone <victormoscone@gmail.com>
2021-03-04 20:07:16 -0800

    Style: Added a set height to the project cards.

--- client/src/components/Project/Project.js
+++ client/src/components/Project/Project.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Project = (props) => {
     return (
         <div>
-            <Card className="bg-dark text-white my-2">
+            <Card className="bg-dark text-white my-2" style={{ height: '35rem' }}>
                 <Card.Img variant="top" src={props.imageSrc} alt={props.imgName} />
                 <Card.Body>
                     <Card.Title>{props.projectTitle}</Card.Title>
