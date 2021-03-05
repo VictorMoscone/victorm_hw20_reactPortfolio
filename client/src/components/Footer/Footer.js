3c489d4c3841185928fd24753ad532cc546fdc52
Victor Moscone <victormoscone@gmail.com>
2021-03-04 19:24:19 -0800

    Feat & Style: Footer now includes copyright disclaimer, styled. Also added footer to a container to keep everything together when shrinking viewport.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import ContainerComp from '../Container/ContainerComp';
 import "./styles.css";
 
-const Footer = ({children}) => {
+const Footer = () => {
     return (
+        <ContainerComp>
         <footer className="footer">
-            {children}
+            <h1 className="footerText">
+                © 2021 Victor Moscone. All Rights Reserved.
+            </h1>
         </footer>
+        </ContainerComp>
+
     );
 };
 
