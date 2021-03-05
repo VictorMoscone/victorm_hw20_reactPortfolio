54b2d8fdeb7e5ed143bc38f58448941e48adf4fc
Victor Moscone <victormoscone@gmail.com>
2021-03-04 19:47:01 -0800

    Refactor: Added more information to our project cards and reorganized them.

--- client/src/App.js
+++ client/src/App.js
@@ -32,9 +32,9 @@ function App() {
             <Project 
               imageSrc="https://cdn.costumewall.com/wp-content/uploads/2019/12/Jotaro-Kujo.jpg" 
               imgName="Jotaro"
-              projectTitle="Homework 13 - Eat-Da-Burger"
-              projectDesc="You're not you when you're hungry. Here, eat-da-burger. This app allows users to create their own burger menu and the devour each burger on the menu. Yum!"
-              github="https://github.com/VictorMoscone/victorm_hw13_burger"/>
+              projectTitle="Project 1 - Hero Fight"
+              projectDesc="Welcome to Team VBV's first fully functional product; Superhero Showdown. Do you like superheroes? Duh! Do you like supervillains? Maybe. What about having Harry Potter fight Lex Luthor? Sure, why not? On the Superhero Matchup page, you'll choose two characters to pit in the ULTIMATE death battle, just to see who would win via arbitrary statistics."
+              github="https://github.com/VictorMoscone/vbv_project1"/>
           </Col>
         </Row>
         
@@ -43,17 +43,17 @@ function App() {
             <Project 
               imageSrc="https://cdn.costumewall.com/wp-content/uploads/2019/12/Jotaro-Kujo.jpg" 
               imgName="Jotaro"
-              projectTitle="Homework 12"
-              projectDesc="Example"
-              github="https://github.com/VictorMoscone/inhumans"/>
+              projectTitle="Homework 13 - Eat-Da-Burger"
+              projectDesc="You're not you when you're hungry. Here, eat-da-burger. This app allows users to create their own burger menu and the devour each burger on the menu. Yum!"
+              github="https://github.com/VictorMoscone/victorm_hw13_burger"/>
           </Col>
           <Col xs lg={3}>
             <Project 
               imageSrc="https://cdn.costumewall.com/wp-content/uploads/2019/12/Jotaro-Kujo.jpg" 
               imgName="Jotaro"
-              projectTitle="Homework 12"
-              projectDesc="Example"
-              github="https://github.com/VictorMoscone/inhumans"/>
+              projectTitle="Homework 12 - Employee Tracker"
+              projectDesc="Management can be tough, much less running an entire business. It sure gets a lot easier when you're able to quickly view, add, and update a back-end spreadsheet of all of your busy-bees. This node app allows you to do just that, with a nice and simple step-by-step process."
+              github="https://github.com/VictorMoscone/victorm_hw12_emptracker"/>
           </Col>
         </Row>
 
@@ -62,17 +62,17 @@ function App() {
             <Project 
               imageSrc="https://cdn.costumewall.com/wp-content/uploads/2019/12/Jotaro-Kujo.jpg" 
               imgName="Jotaro"
-              projectTitle="Homework 12"
-              projectDesc="Example"
-              github="https://github.com/VictorMoscone/inhumans"/>
+              projectTitle="Homework 11 - Note Taker"
+              projectDesc="This app exists to allow you, the ever busy gentleman or woman, to write down your innermost thoughts onto the internet. Brilliant! You can take notes, view old notes, and maybe even delete them."
+              github="https://github.com/VictorMoscone/victorm_hw11_notetaker"/>
           </Col>
           <Col xs lg={3}>
             <Project 
               imageSrc="https://cdn.costumewall.com/wp-content/uploads/2019/12/Jotaro-Kujo.jpg" 
               imgName="Jotaro"
-              projectTitle="Homework 12"
-              projectDesc="Example"
-              github="https://github.com/VictorMoscone/inhumans"/>
+              projectTitle="Homework 10 - Team Generator"
+              projectDesc="Are you here to form your ultimate fantasy engineering team? Well, look no further! For the low-low price of $0, you can quicky build your own team website with little-to-no effort! Just answer some simple prompts and you'll be off to the races."
+              github="https://github.com/VictorMoscone/victorm_hw10_teamGen"/>
           </Col>
         </Row>
       </ContainerComp>
